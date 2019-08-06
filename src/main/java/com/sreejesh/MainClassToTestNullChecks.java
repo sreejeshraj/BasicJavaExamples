8f1052e195da0864f78fc1f431383aaf5f42fa29
admin@WindowsLaptop <admin@WindowsLaptop>
2019-08-06 23:48:57 +0530

    After adding mill library to check nulls

--- src/main/java/com/sreejesh/MainClassToTestNullChecks.java
+++ src/main/java/com/sreejesh/MainClassToTestNullChecks.java
@@ -2,6 +2,9 @@ package com.sreejesh;
 
 import java.util.Objects;
 
+import com.scottshipp.code.mill.NullSafe;
+
+import lombok.Data;
 import lombok.NonNull;
 
 public class MainClassToTestNullChecks {
@@ -13,7 +16,8 @@ public class MainClassToTestNullChecks {
 		//String sampleString = null;
 		int lengthOfString = getLengthOfString(sampleString);
 		System.out.println("lengthOfString: "+lengthOfString);
-		nullCheckWithinAMethod();
+		testNestedNullChecksInObjects();
+		//nullCheckWithinAMethod();
 		System.out.println("Exiting Main!");
 
 	}
@@ -23,6 +27,29 @@ public class MainClassToTestNullChecks {
 		
 		return stringOne.length();
 	}
+	
+	private static void testNestedNullChecksInObjects()
+	{
+		Child childOne = new Child();
+		childOne.setChildValue(100);
+		Parent parentOne = new Parent();
+		parentOne.setChild(childOne);
+		
+		Integer childOneValue = NullSafe.of(parentOne)
+						                .call(Parent::getChild)
+						                .call(Child::getChildValue)
+						                .get();
+	    System.out.println("childOneValue: "+childOneValue);
+	    
+	    Parent parentWithNullChild = new Parent();
+		parentOne.setChild(null);
+		Integer childTwoValue = NullSafe.of(parentWithNullChild)
+						                .call(Parent::getChild)
+						                .call(Child::getChildValue)
+						                .get();
+		System.out.println("childTwoValue: "+childTwoValue);
+		
+	}
 
 	private static void nullCheckWithinAMethod() {
 		String stringTwo;
@@ -30,7 +57,21 @@ public class MainClassToTestNullChecks {
 		//stringTwo = "SomeString";
 		//Testing For stringTwo (local variable within a method)
 		Objects.requireNonNull(stringTwo, "stringTwo cannot be null!");
-		int stringTwoLength = stringTwo.length();
+		Integer stringTwoLength = stringTwo.length();
+		System.out.println("stringTwoLength: "+stringTwoLength);
+	}
+	
+	
+	@Data
+	static class Parent
+	{
+		Child child;
+	}
+	
+	@Data
+	static class Child
+	{
+		Integer childValue;
 	}
 
 }
